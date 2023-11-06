fe066887f91534a8e4449dd64d7c97c4d8b619d6
Lenni009 <guentherhanslphdyt@gmail.com>
2023-11-06 15:26:41 +0100

    break up query syntax

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -7,21 +7,34 @@ const props = defineProps<{
   filter: string;
 }>();
 
-const censusQuery =
-  'https://nomanssky.fandom.com/api.php?action=cargoquery&format=json&origin=*&limit=500&tables=Bases&fields=Name%2C%20CensusPlayer%2C%20CensusRenewal&where=CensusShow%20IS%20NOT%20NULL%20AND%20Civilized%3D%22Galactic%20Hub%20Eissentam%22&order_by=CensusRenewal';
+const apiPath = 'https://nomanssky.fandom.com/api.php';
+const action = 'cargoquery';
+const format = 'json';
+const origin = '*';
+const limit = '500';
+const tables = 'Bases';
+const fields = ['Name', 'CensusPlayer', 'CensusRenewal'];
+const civilized = 'Eisvana';
+const where = `CensusShow IS NOT NULL AND Civilized="${civilized}"`;
+const orderBy = 'CensusRenewal';
+
+const censusQuery = `${apiPath}?action=${action}&format=${format}&origin=${origin}&limit=${limit}&tables=${tables}&fields=${fields.join(
+  '%2C%20'
+)}&where=${where}&order_by=${orderBy}`;
 
 const censusData = ref<QueryEntry[]>([]);
 const requestFailed = ref<boolean>(false);
 
-onMounted(() =>
-  fetch(censusQuery)
-    .then((r) => r.json())
-    .then((j) => (censusData.value = j.cargoquery))
-    .catch((e) => {
-      console.warn(e);
-      requestFailed.value = true;
-    })
-);
+onMounted(async () => {
+  try {
+    const res = await fetch(censusQuery);
+    const data = await res.json();
+    censusData.value = data.cargoquery;
+  } catch (e) {
+    console.warn(e);
+    requestFailed.value = true;
+  }
+});
 
 const filteredCensusData = computed(() =>
   censusData.value.filter((item) => item.title.CensusPlayer.toLowerCase().includes(props.filter.toLowerCase()))
