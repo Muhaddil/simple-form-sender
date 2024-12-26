43ae5cb6853be954dd0a286f1aec245a5f1813db
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-12-26 02:53:59 +0100

    Update 0.0.61 - Updated build inputs to include SAFD forms

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
-	plugins: [
-		vue(),
-	],
+  base: './',
+  plugins: [vue()],
   build: {
     target: 'ESNext',
     rollupOptions: {
-      input: ['index.html', 'form1.html', 'form2.html', 'form3.html', 'trasladosform.html'],
+      input: [
+        'index.html',
+        'form1.html',
+        'form2.html',
+        'form3.html',
+        'trasladosform.html',
+        'SAFDForm1.html',
+        'SAFDForm2.html',
+        'SAFDForm3.html',
+      ],
     },
   },
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		}
-	}
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
